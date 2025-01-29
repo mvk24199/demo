517b4ae808e5cf9196c9871fcb3d2ebc65b80da5
mvk24199 <madavava@mail.uc.edu>
2025-01-29 15:34:49 -0500

    Created Calcultaor.Java

---
+++ src/main/java/com/example/Calculator.java
@@ -0,0 +1,10 @@
+package com.example;
+public class Calculator {
+    public static void main(String[] args) {
+        public int add ( int a, int b){
+            return a+b;
+        }
+        public int multiply(int a, int b){
+            return a*b;
+        }
+}
