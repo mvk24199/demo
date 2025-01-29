a5c1606f1bd5686030a44cf1a054fa37d497f249
mvk24199 <madavava@mail.uc.edu>
2025-01-29 15:48:22 -0500

    Add Junit tests for Calculator

---
+++ src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest {
+    @Test
+    public void testAdd(){
+        Calculator calc = new Calculator();
+        asserEquals("Should return 5",5,calc.add(2,3));
+    }
+
+    @Test
+    public void testMulitply(){
+        Calculator calc = new Calculator();
+        asserEquals("Should return 6",6,calc.add(2,3));
+    }
+}
